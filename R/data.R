5b5db7d5aea4e204aa1f2f4c08efdbca0b0c6972
Marek Gierlinski <Marek.Gierlinski@gmail.com>
2024-04-03 13:32:49 +0100

    Working on docs

--- R/data.R
+++ R/data.R
@@ -3,35 +3,35 @@
 #' A subset of 6 + 6 replicates was selected from data set reported in
 #' https://doi.org/10.1093/bioinformatics/btv425
 #'
-#' @usage data(data)
+#' @usage data(yeast_data)
 #' @format A tibble with 3 columns
 #' @return Expression data from a yeast RNA-seq experiment.
-"data"
+"yeast_data"
 
 #' Experimental design from an yeast RNA-seq experiment.
 #'
 #' A subset of 6 + 6 replicates was selected from data set reported in
 #' https://doi.org/10.1093/bioinformatics/btv425
 #'
-#' @usage data(metadata)
+#' @usage data(yeast_metadata)
 #' @format A tibble with 3 columns
 #' @return Design of the experiment containing expression `data` and
 #'   differential expression `de`.
-"metadata"
+"yeast_metadata"
 
 #' Differential expression results for yeast RNA-seq.
 #'
 #' A subset of 6 + 6 replicates was selected from data set reported in
 #' https://doi.org/10.1093/bioinformatics/btv425
 #'
-#' @usage data(de)
+#' @usage data(yeast_de)
 #' @format A tibble with 6 columns
 #' @return Results for differential expression for yeast RNA-seq.
-"de"
+"yeast_de"
 
 #' Feature description for an example RNA-seq experiment.
 #'
-#' @usage data(features)
+#' @usage data(yeast_features)
 #' @format A tibble with 6 columns
 #' @return Feature description table.
-"features"
+"yeast_features"
